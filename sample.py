821b1d057bc972ecbefabde4ff70130ce54ece36
Mete Demirtas <metedemirtas26@gmail.com>
2022-07-17 12:46:30 +0200

    sad

--- sample.py
+++ sample.py
@@ -8,8 +8,6 @@ password = 'MyPass@word'
 conn = pyodbc.connect('DRIVER={ODBC Driver 18 for SQL Server};SERVER={172.17.0.1,1433};DATABASE=tempdb;UID=sa;PWD=MyPass@word;TrustServerCertificate=yes;')
 
 cursor = conn.cursor()
-cursor.execute("""IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='tablename' AND xtype='U')
-        CREATE TABLE tablename (id INTEGER PRIMARY KEY, fieldA NVARCHAR(Max) NOT NULL, fieldB NVARCHAR(Max) NOT NULL)""")
 cursor.execute("""
 CREATE TABLE Test_table (id INT, name VARCHAR(50))
 INSERT into Test_table VALUES (1, 'test')

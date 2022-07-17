5a2ee4131bc36977de8d8facf16a3fb6c63de7d0
Mete Demirtas <metedemirtas26@gmail.com>
2022-07-17 12:50:36 +0200

    qwer

--- python-start.sh
+++ python-start.sh
@@ -1,11 +1,8 @@
 echo "start"
 
-docker image build . --platform=linux/amd64 -t flask_docker_image || true
+docker image build . -t flask_docker_image || true
 
-docker stop flask-web || true
-docker rm flask-web || true
-
-docker container run --platform=linux/amd64 --network="host" --name flask-web \
+docker container run --network="host" \
 	flask_docker_image:latest
 
 echo "end"
